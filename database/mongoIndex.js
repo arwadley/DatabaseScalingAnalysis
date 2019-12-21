9b88f5183a14d34265ae6246e746df4979442e32
arwadley <wadley.alyssa@gmail.com>
2019-12-21 11:33:11 -0600

    server set up with mongo connection. Get and Delete routes working with postman.

--- database/mongoIndex.js
+++ database/mongoIndex.js
@@ -1,17 +1,44 @@
-// const MongoClient = require("mongodb").MongoClient;
-// const assert = require("assert");
+var MongoClient = require("mongodb").MongoClient;
 
-// const url = "mongodb://localhost:27017";
+const url = "mongodb://localhost:27017";
 
-// const dbName = "m_gamazon_products";
+const dbName = "m_gamazon_products";
 
-// const client = new MongoClient(url);
+const client = new MongoClient(url);
 
-// var database;
+let db;
+let collection;
 
-// client.connect(function(error, client) {
-//   assert.equal(null, err);
-//   console.log("Connected correctly to server");
+client.connect(function(error, client) {
+  if (error) {
+    console.log(error);
+  } else {
+    db = client.db(dbName);
+    collection = db.collection("product_description");
+  }
+});
 
-//   database = client.db(dbName);
-// });
+let addItem = function(item, callback) {
+  collection.insertOne(item, function(error, result) {
+    callback(error, result);
+  });
+};
+
+let getItemById = function(currentId, callback) {
+  currentId = Number(currentId);
+  collection.findOne({ id: currentId }, function(error, result) {
+    callback(error, result);
+  });
+};
+
+let deleteItemById = function(currentId, callback) {
+  currentId = Number(currentId);
+  collection.deleteOne({ id: currentId }, function(error, result) {
+    callback(error, result);
+  });
+};
+
+module.exports.collection = collection;
+module.exports.addItem = addItem;
+module.exports.getItemById = getItemById;
+module.exports.deleteItemById = deleteItemById;
--- server/index.js
+++ server/index.js
@@ -3,67 +3,87 @@ const path = require("path");
 let app = express();
 const faker = require("faker");
 const port = 7777;
-const dbPG = require("../database/postgresIndex.js");
+// const dbPG = require("../database/postgresIndex.js");
 let fs = require("fs");
+let mDB = require("../database/mongoIndex");
 
 app.use(express.static(path.join(__dirname, "../public/dist")));
 app.use(express.json());
 
-app.get("/item", (req, res) => {});
+app.get("/item", (req, res) => {
+  let id = req.query.id;
+  mDB.getItemById(id, function(error, result) {
+    if (error) {
+      res.status(404).send();
+    } else {
+      res.status(200).send(result);
+    }
+  });
+});
 
 app.put("/item", (req, res) => {});
 
-app.post("/item", (req, res) => {});
+app.post("/item", (req, res) => {
+  let product = req.query;
+});
 
-app.delete("/item", (req, res) => {});
-
-app.get("/fs", (req, res) => {
-  let string =
-    "id, productname, productmaker, productdesc, productprice, productrating, productnumofratings, productnumofquestionsanswered, productcategory + \n";
-  for (let i = 9500000; i < 10000000; i++) {
-    let id = i;
-    let productName = faker.random.word();
-    let productMaker = faker.random.words(2);
-    let productDesc = faker.random.words(10);
-    let productPrice = (i % 500) * 1.01;
-    let productRating = (i % 5) * 1.0;
-    let productNumOfRatings = (i % 500) + 12;
-    let productNumOfQuestionsAnswered = (i % 300) + 3;
-    let productCategory = faker.random.words(2);
-    string +=
-      id +
-      ";" +
-      productName +
-      ";" +
-      productMaker +
-      ";" +
-      productDesc +
-      ";" +
-      productPrice +
-      ";" +
-      productRating +
-      ";" +
-      productNumOfRatings +
-      ";" +
-      productNumOfQuestionsAnswered +
-      ";" +
-      productCategory +
-      "\n";
-  }
-  let csvFileName = __dirname + "csvData20.csv";
-  fs.writeFile(csvFileName, string, function(err, data) {
-    if (err) {
-      console.log("oh no!");
-      lastUploaded = csvFileName;
+app.delete("/item", (req, res) => {
+  let id = req.query.id;
+  mDB.deleteItemById(id, function(error, result) {
+    if (error) {
+      console.log(error);
       res.status(400).end();
     } else {
-      console.log(csvFileName);
-      res.status(200).end();
+      res.status(200).send(result);
     }
   });
 });
 
 app.listen(port, console.log(`Listening on port ${port}...`));
-/Users/alyssawadley/Documents/GitHub/product-description/servercsvData01.csv
 
-mongoimport -d m_gamazon_products -c product_description --type csv --headerline --file /Users/alyssawadley/Documents/GitHub/product-description/servercsvData20.csv
+// helper function used to create data and write to csv files
+// app.get("/fs", (req, res) => {
+//   let string =
+//     "id, productname, productmaker, productdesc, productprice, productrating, productnumofratings, productnumofquestionsanswered, productcategory + \n";
+//   for (let i = 9500000; i < 10000000; i++) {
+//     let id = i;
+//     let productName = faker.random.word();
+//     let productMaker = faker.random.words(2);
+//     let productDesc = faker.random.words(10);
+//     let productPrice = (i % 500) * 1.01;
+//     let productRating = (i % 5) * 1.0;
+//     let productNumOfRatings = (i % 500) + 12;
+//     let productNumOfQuestionsAnswered = (i % 300) + 3;
+//     let productCategory = faker.random.words(2);
+//     string +=
+//       id +
+//       ";" +
+//       productName +
+//       ";" +
+//       productMaker +
+//       ";" +
+//       productDesc +
+//       ";" +
+//       productPrice +
+//       ";" +
+//       productRating +
+//       ";" +
+//       productNumOfRatings +
+//       ";" +
+//       productNumOfQuestionsAnswered +
+//       ";" +
+//       productCategory +
+//       "\n";
+//   }
+//   let csvFileName = __dirname + "csvData20.csv";
+//   fs.writeFile(csvFileName, string, function(err, data) {
+//     if (err) {
+//       console.log("oh no!");
+//       lastUploaded = csvFileName;
+//       res.status(400).end();
+//     } else {
+//       console.log(csvFileName);
+//       res.status(200).end();
+//     }
+//   });
+// });
